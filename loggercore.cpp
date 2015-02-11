8628eb0f72846c73f41eb3c18545bf8e8b673f64
Maxim Vasilchuk <mvasilchuk@gmail.com>
2015-02-11 22:05:28 +0200

    Rewrited logger core. Disabled debug output for release by default. Added --verbose key to show debug output even in release.

--- loggercore.cpp
+++ loggercore.cpp
@@ -1,6 +1,9 @@
 #include "core.h"
 #include "loggercore.h"
 
+#include <cstdio>
+#include <QDateTime>
+
 #define LOG_TYPE_DEBUG QtDebugMsg
 #define LOG_TYPE_WARN QtWarningMsg
 #define LOG_TYPE_CRITICAL QtCriticalMsg
@@ -30,6 +33,12 @@ LoggerCore::LoggerCore(QObject *parent) :
 
 void LoggerCore::MessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
+    #ifdef QT_DEBUG
+        static bool verboseOutput = true;
+    #else
+        static bool verboseOutput = qApp->arguments().contains("--verbose");
+    #endif
+
     /**
      * @brief localMsg
      *
@@ -74,79 +83,149 @@ void LoggerCore::MessageHandler(QtMsgType type, const QMessageLogContext &contex
 
     const char* message = localMsg.constData();
 
+    QString line = "";
+
+#ifdef EXTRA_DEBUG_INFO
+    if(context.file != NULL)
+        line = QString("(%1:%2) ").arg(context.file).arg(QString::number(context.line));
+#endif
+
+    const char* current_time = QDateTime::currentDateTime().toString("hh:mm:ss:zzz").toUtf8().constData();
+
+    FILE* output_channel = stdout;
+    QString output_line;
+
+
+
     switch (msgType) {
         case LOG_TYPE_DEBUG:
         {
-#ifdef EXTRA_DEBUG_INFO
-            fprintf(stdout, "[DEBUG] (%s:%u): %s\n", context.file, context.line, message);
-#else
-            fprintf(stdout, "[DEBUG] %s\n", message);
-#endif
+            if(verboseOutput)
+            {
+                output_line = QString("[DEBUG][%1] %2%3\n").arg(current_time).arg(line).arg(message);
+            }
             break;
         }
         case LOG_TYPE_WARN:
         {
-#ifdef EXTRA_DEBUG_INFO
-            fprintf(stdout, "[WARN ] (%s:%u): %s\n", context.file, context.line, message);
-#else
-            fprintf(stdout, "[WARN ] %s\n", message);
-#endif
+            output_line = QString("[WARN ][%1] %2%3\n").arg(current_time).arg(line).arg(message);
             break;
         }
         case LOG_TYPE_CRITICAL:
         {
-            fprintf(stderr, "[CRIT ]: (%s:%u): %s\n", context.file, context.line, message);
+            output_channel = stderr;
+            output_line = QString("[CRIT ][%1] %2%3\n").arg(current_time).arg(line).arg(message);
             Core::printCallStack();
             break;
         }
         case LOG_TYPE_FATAL:
         {
-            fprintf(stderr, "[FATAL] (%s:%u): %s\n", context.file, context.line, message);
+            output_channel = stderr;
+            output_line = QString("[FATAL][%1] %2%3\n").arg(current_time).arg(line).arg(message);
             Core::printCallStack();
             abort();
         }
         case LOG_TYPE_STUB:
         {
-#ifdef EXTRA_DEBUG_INFO
-            fprintf(stdout, "[STUB ] (%s:%u): %s%s%s\n", context.file, context.line, context.function, strlen(message) > 0 ? ": " : "", message);
-#else
-            fprintf(stdout, "[STUB ] %s%s%s\n", context.function, strlen(message) > 0 ? ": " : "", message);
-#endif
-
+            if(context.function != NULL || !(message == NULL || strlen(message) == 0))
+            {
+                output_line = QString("[STUB ][%1] %2%3%4%5\n").arg(current_time).arg(line).arg(context.function).arg(strlen(message) > 0 ? ": " : "", message);
+            }
             break;
         }
         case LOG_TYPE_LOG:
         {
-#ifdef EXTRA_DEBUG_INFO
-            fprintf(stdout, "[LOG  ] (%s:%u): %s\n", context.file, context.line, message);
-#else
-            fprintf(stdout, "[LOG  ] %s\n", message);
-#endif
-
+            output_line = QString("[LOG  ][%1] %2%3\n").arg(current_time).arg(line).arg(message);
             break;
         }
         case LOG_TYPE_FIXME:
         {
-            fprintf(stdout, "[FIXME] (%s:%u): %s\n", context.function, context.line, message);
+            output_line = QString("[FIXME][%1] (%2:%3): %4\n").arg(current_time).arg(context.function).arg(QString::number(context.line)).arg(message);
             break;
         }
         case LOG_TYPE_BUG:
         {
-            fprintf(stderr, "[BUG  ] (%s:%u) %s -> %s\n", context.file, context.line, context.function, message);
+            output_channel = stderr;
+            output_line = QString("[BUG  ][%1] (%2:%3) %4 -> %5\n").arg(current_time).arg(context.file).arg(context.line).arg(context.function).arg(message);
             break;
         }
         case LOG_TYPE_WTF:
         {
-            fprintf(stderr, "[WTF  ] (%s:%u) %s -> %s\n", context.file, context.line, context.function, message);
+            output_channel = stderr;
+            output_line = QString("[WTF  ][%1] (%2:%3) %4 -> %5\n").arg(current_time).arg(context.file).arg(QString::number(context.line)).arg(context.function).arg(message);
             break;
         }
         default:
         {
-            fprintf(stdout, "[OTHER] (%s:%u | %s): %s\n", context.file, context.line, context.function, message);
+            output_line = QString( "[OTHER][%1] (%2:%3 | %4): %5\n").arg(current_time).arg(context.file).arg(QString::number(context.line)).arg(context.function).arg(message);
         }
 
     }
 
+#ifdef QT_DEBUG
+    static bool colorOutput = false;
+#else
+    static bool colorOutput = qApp->arguments().contains("--color");
+#endif
+
+    if(colorOutput)
+    {
+        // Sometimes crashes the app on exit
+        //output_line = colorize(output_line);
+    }
+
+    fprintf(output_channel, "%s", output_line.toUtf8().constData());
+
     fflush(stdout);
     fflush(stderr);
+}
+
+QString LoggerCore::colorize(const QString &str)
+{
+    static int debugColor = 36;
+    static int stubColor = 32;
+    static int logColor = 37;
+    static int errorColor = 31;
+    static int infoColor = 35;
+    //static int methodColor = 33;
+    //static int methodParamsColor  = 32;
+    //static int normalColor = 39;
+
+    static QRegExp debugRegExp = QRegExp("^(?:\\[)(DEBUG)(?:\\s*\\])");
+    static QRegExp stubRegExp = QRegExp("^(?:\\[)(STUB)(?:\\s*\\])");
+    static QRegExp bugRegExp = QRegExp("^(?:\\[)(BUG)(?:\\s*\\])");
+    static QRegExp warnRegExp = QRegExp("^(?:\\[)(WARN)(?:\\s*\\])");
+    static QRegExp logRegExp = QRegExp("^(?:\\[)(LOG)(?:\\s*\\])");
+    static QRegExp errorRegExp = QRegExp("^(?:\\[)(ERROR)(?:\\s*\\])");
+    static QRegExp fatalRegExp = QRegExp("^(?:\\[)(FATAL)(?:\\s*\\])");
+    static QRegExp infoRegExp = QRegExp("^(?:\\[)(INFO)(?:\\s*\\])");
+    //static QRegExp methodCallRegExp = QRegExp("(\\w+)(::)(\\w+)(?:(\\s*\\()(.*)(\\))){0,1}");
+
+    try {
+        QString result = str;
+        result.replace(debugRegExp, Colorizer::format("[DEBUG]", debugColor));
+        result.replace(stubRegExp,  Colorizer::format("[STUB ]", stubColor));
+        result.replace(bugRegExp,   Colorizer::format("[BUG  ]", stubColor));
+        result.replace(warnRegExp,  Colorizer::format("[WARN ]", stubColor));
+        result.replace(logRegExp,   Colorizer::format("[LOG  ]", logColor));
+        result.replace(errorRegExp, Colorizer::format("[ERROR]", errorColor));
+        result.replace(fatalRegExp, Colorizer::format("[FATAL]", errorColor));
+        result.replace(infoRegExp,  Colorizer::format("[INFO ]", infoColor));
+
+        /*QString methodCall = QString("%1%2%3%4%5%6")
+                .arg(Colorizer::format("\\1", methodColor))         // class
+                .arg(Colorizer::format("\\2", stubColor))           // ::
+                .arg(Colorizer::format("\\3", methodColor))         // methodName
+                .arg(Colorizer::format("\\4", normalColor))         // (
+                .arg(Colorizer::format("\\5", methodParamsColor))   // params
+                .arg(Colorizer::format("\\6", normalColor));        // )*/
+
+        //result.replace(methodCallRegExp, methodCall);
+        return result;
+    }
+    catch(std::exception e)
+    {
+        fprintf(stderr, "%s", e.what());
+    }
+    return str;
 }
--- loggercore.h
+++ loggercore.h
@@ -2,7 +2,7 @@
 #define LOGGERCORE_H
 
 #include <QObject>
-
+#include <QRegExp>
 
 class LoggerCore : public QObject
 {
@@ -12,9 +12,23 @@ public:
 
 signals:
 
+protected:
+
+
 public slots:
 
     static void MessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+protected:
+    static QString colorize(const QString &str);
+
+    class Colorizer: public QString {
+        QString data;
+    public:
+        static QString format(const QString &str, int color)
+        {
+            return QString("\033[%1m%2\033[0m").arg(color).arg(str);
+        }
+    };
 };
 
 #endif // LOGGERCORE_H
